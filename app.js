a94e3c84282b8705b01dbb04c5e2141abedf00c0
Heikki Malkavaara <heimal@student.uef.fi>
2023-05-15 10:34:24 +0300

    refactoring to use app.js for middlewares

---
+++ app.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const Person = require('./models/person')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', false)
+
+const url = config.MONGODB_URI
+
+logger.info('connecting to', url)
+mongoose.connect(url)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(express.static('build'))
+app.use(morgan(function (tokens, req, res) {
+  let logged = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+
+  if (req.method === 'POST'){
+    logged = logged.concat(' ')
+    logged = logged.concat(JSON.stringify(req.body))
+  }
+  return logged
+}))
+
+// koko puhelinluettelon näyttäminen json-muodossa
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(people => {
+    res.json(people)
+  })
+})
+
+// info-ruudun esittäminen
+app.get('/info', (req, res) => {
+  const date = new Date()
+  Person.count({}).then(count => {
+    res.send(`Phonebook has info for ${count} people <br> ${date}`)
+  })
+})
+
+// yksittäisen henkilön esittäminen
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  }).catch(error => next(error))
+})
+
+// yksittäisen henkilön poisto
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// henkilön lisääminen
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
+// henkilön numeron muokkaaminen
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  Person.findByIdAndUpdate(req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' })
+    .then(result => {
+      res.json(result)
+    }).catch(error => next(error))
+})
+
+// virheidenkäsittelijät
+const uknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'uknown endpoint' })
+}
+
+app.use(uknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+module.exports = app
